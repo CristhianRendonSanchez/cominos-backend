bf8064b608c2e2740346b4c59ce683519f6d5554
luiseduardo23 <luis.padilla@correounivalle.edu.co>
2022-01-20 22:10:16 -0500

    feat: CRUD del cliente, creación de rutas del cliente para la ApI.
    otros: creación de un bodyparser para las solicitudes POST

---
+++ src/modules/cliente/routes/cliente.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import clienteControllers from '../controllers/cliente.controller';
+
+class ClienteRoutes {
+  router: Router;
+
+  constructor() {
+    this.router = Router();
+    this.routes();
+  }
+
+  routes() {
+    this.router.get('/GetCliente/:cedula', clienteControllers.GetCliente);
+    this.router.post('/PostCliente', clienteControllers.PostCliente);
+    this.router.put('/PutCliente', clienteControllers.PutCliente);
+    this.router.delete('/DeleteCliente', clienteControllers.DeleteCliente);
+  }
+}
+
+const clienteRoutes = new ClienteRoutes();
+export default clienteRoutes.router;
